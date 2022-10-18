1cb3387143c7c3e79c9f64e20504fbeaff7bb19e
kithJames <echejamesmathew@yahoo.com>
2022-10-17 19:55:12 -0700

    printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,33 @@
+
+
+#include <stdio.h>
+
+/*
+*
+*main - entry point
+*Return: zero
+*/
+int main(void)
+{	int num = 48;
+
+	while (num <= 57)
+
+	{
+
+		putchar; num;
+
+		num++;
+}
+	putchar('\n');
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
