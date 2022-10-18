05983a49905a53f97c5c4e9b0608058fb2119551
kithJames <echejamesmathew@yahoo.com>
2022-10-17 20:08:12 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,35 @@
+
+
+#include <stdio.h>
+
+/*
+*
+*main - entry point
+*Description prints numbers
+*Return: zero
+*/
+int
+main(void)
+{	int num = 48;
+
+	while (num <= 57)
+
+	{
+
+		putchar(num);
+
+		num++;
+}
+	putchar('\n');
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
