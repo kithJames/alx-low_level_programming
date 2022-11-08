db9e9caba67dfafe69713bb2fc33151354479b58
kithJames <echejamesmathew@yahoo.com>
2022-11-08 01:41:20 -0800

    hot fix

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,12 @@
-int _putchar(char c);
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
 
 char *create_array(unsigned int size, char c);
+
+int _putchar(char c);
 
 char *_strdup(char *str);
 
@@ -11,3 +17,7 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 
 char *argstostr(int ac, char **av);
+
+
+
+#endif 
