699a7d38323edd5ade80d74476d346fee1febec0
kithJames <echejamesmathew@yahoo.com>
2022-11-10 12:10:55 -0800

    go

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,9 +8,9 @@
  */
 void *malloc_checked(unsigned int b);
 {
-	void *pointer = malloc(b);
+	void *ptr = malloc(b);
 
-	if (pointer == NULL)
+	if (ptr == NULL)
 		exit(98);
-	return (pointer);
+	return (ptr);
 }
