10106a3c96d31801a979d2ed5ac0c1ed1281b982
kithJames <echejamesmathew@yahoo.com>
2022-11-25 17:16:52 -0800

    ->

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/*
+ * list_len - returns number of elements in linked list
+ * @h: points to the list_t list
+ * Return: number of element
+ */
+size_t
+list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while
+	{
+		n++
+		h = h->next;
+	}
+	return (n);
+}
